c0aad81908d6e42d11df041acd9197b5459e3744
wdev-jahidhasan <wdev.jahidhasan@gmail.com>
2026-05-05 17:33:48 +0600

    json data added

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <Carousel></Carousel>
+    <h1 className="text-center font-bold text-5xl">Popular Products</h1>
+    <h1 className="text-center font-bold text-5xl text-green-300 my-5">Summer Care Tips</h1>
+    <h1 className="text-center font-bold text-5xl text-purple-400 my-5">Top Brands</h1>
+    </>
   );
 }
